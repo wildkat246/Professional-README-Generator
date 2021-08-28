ceb38410d6f6cf859af70e0946ce3951bc1e1ae0
wildkat246 <85767537+wildkat246@users.noreply.github.com>
2021-08-27 22:06:21 -0400

    basic layout

---
+++ index.js
@@ -0,0 +1,30 @@
+// TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+  
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
